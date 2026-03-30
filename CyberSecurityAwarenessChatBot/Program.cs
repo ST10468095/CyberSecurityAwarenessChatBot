8a81d378fdf45059a0497a18129af2c63cbf9270
Nathan Beaumont <ST10468095@myemeris.edu.za>
2026-03-30 11:34:35 +0200

    User Model added for greeting

--- CyberSecurityAwarenessChatBot/Program.cs
+++ CyberSecurityAwarenessChatBot/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CyberSecurityAwarenessChatBot.Utils;
+using CyberSecurityAwarenessChatBot.Models;
+
 
 namespace CyberSecurityAwarenessChatBot
 {
@@ -12,6 +14,14 @@ namespace CyberSecurityAwarenessChatBot
         static void Main(string[] args)
         {
             AsciiArt.ShowLogo();
+            User user = new User();
+
+            Console.Write("\nEnter your name: ");
+            user.Name = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Welcome {user.Name}! I will be helping you stay safe today!");
+            Console.ResetColor();
         }
     }
 }
