35ce5fe3d55c34aac100fa010b1e2ddb99c17322
Nathan Beaumont <ST10468095@myemeris.edu.za>
2026-03-30 11:43:46 +0200

    Conversation looping has been added

--- CyberSecurityAwarenessChatBot/Program.cs
+++ CyberSecurityAwarenessChatBot/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using CyberSecurityAwarenessChatBot.Models;
+using CyberSecurityAwarenessChatBot.Services;
+using CyberSecurityAwarenessChatBot.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CyberSecurityAwarenessChatBot.Utils;
-using CyberSecurityAwarenessChatBot.Models;
 
 
 namespace CyberSecurityAwarenessChatBot
@@ -22,6 +23,33 @@ namespace CyberSecurityAwarenessChatBot
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Welcome {user.Name}! I will be helping you stay safe today!");
             Console.ResetColor();
+
+            string input;
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\nYou: ");
+                Console.ResetColor();
+
+                input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Please type something.");
+                    continue;
+                }
+
+                if (input.ToLower() == "exit")
+                    break;
+
+                string response = ChatbotService.GetResponse(input);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Bot: " + response);
+                Console.ResetColor();
+            }
         }
+
     }
 }
