e442a6d1abf9a96456c8d51ceef83abaf3b54787
Nathan Beaumont <ST10468095@myemeris.edu.za>
2026-03-30 11:40:46 +0200

    ChatBot service created with responds to prompts from user

---
+++ CyberSecurityAwarenessChatBot/Services/ChatbotService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurityAwarenessChatBot.Services
+{
+    public class ChatbotService
+    {
+        public static string GetResponse(string input)
+        {
+            input = input.ToLower();
+
+            if (input.Contains("password"))
+                return "Use a strong password with uppercase letters, numbers, and symbols.";
+
+            if (input.Contains("phishing"))
+                return "Never click suspicious email links.Ever!";
+
+            if (input.Contains("browsing"))
+                return "Always check HTTPS websites before entering personal details.";
+
+            if (input.Contains("purpose"))
+                return "I help users stay safe and provide cybersecurity awareness.";
+
+            if (input.Contains("how are you"))
+                return "I'm ready to help!";
+
+            return "I don't understand. Try asking about something else related to cybersecurity.";
+        }
+    }
+}
