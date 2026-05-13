6d70e4ce559235509f0f072ced29aad9ae372899
ST10468095 <ST10468095@vcconnect.edu.za>
2026-05-13 16:06:34 +0200

    Designed GUI Layout

---
+++ CyberSecurityAwarenessChatBot_GUI/ChatMessage.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurityAwarenessChatBot_GUI
+{
+    public class ChatMessage
+    {
+        public string Message { get; set; }
+        public bool IsUser { get; set; }
+    }
+}
